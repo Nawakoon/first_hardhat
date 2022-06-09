a36f59536b8503fa35bbb60d105d437b5b49bb2c
Nawakoon Peerarasee <fame.8@hotmail.com>
2022-06-09 14:31:12 +0700

    build finish deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,35 @@
 // import
+const { verifyMessage } = require("ethers/lib/utils")
+const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
-    console.log("hello world")
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+  console.log("Deploying contract")
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.deployed()
+  console.log(`Deployed contract to: ${simpleStorage.address}`)
+  // see what happens when we deploy to hardhat local network
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...")
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Currennt Value is: ${currentValue}`)
+
+  // Update the current value
+  const transactionResponse = await simpleStorage.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Update Value is: ${updatedValue}`)
 }
 
 // main
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
