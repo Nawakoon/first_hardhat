742599638a3980dc95efd8a19e508d5001328df5
Nawakoon Peerarasee <fame.8@hotmail.com>
2022-06-11 15:52:28 +0700

    build Simple and test_deploy

--- scripts/test_deploy.js
+++ scripts/test_deploy.js
@@ -3,34 +3,38 @@ const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
-    const SimpleStorageFactory = await ethers.getContractFactory(
-        "SimpleStorage"
-    )
+    const SimpleFactory = await ethers.getContractFactory("Simple")
     console.log("Deploying contract")
-    const simpleStorage = await SimpleStorageFactory.deploy()
+    const simple = await SimpleFactory.deploy()
     // if we didn't put any agrument
     // hardhat will run this function in defaultNetwork -> hardhat network
     // work like $ yarn hardhat run scripts/deploy.js --network hardhat
-    await simpleStorage.deployed()
-    console.log(`Deployed contract to: ${simpleStorage.address}`)
+    await simple.deployed()
+    console.log(`Deployed contract to: ${simple.address}`)
     // see what happens when we deploy to hardhat local network
     if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
         console.log("Waiting for block confirmations...")
-        await simpleStorage.deployTransaction.wait(6)
-        await verify(simpleStorage.address, [])
+        await simple.deployTransaction.wait(6)
+        // [] mean the lastest address
+        await verify(simple.address, [])
     }
 
-    const currentValue = await simpleStorage.retrieve()
+    const currentValue = await simple.retrieve()
     console.log(`Currennt Value is: ${currentValue}`)
 
     // Update the current value
-    const transactionResponse = await simpleStorage.store(7)
+    const transactionResponse = await simple.store(100)
     await transactionResponse.wait(1)
-    const updatedValue = await simpleStorage.retrieve()
+    const updatedValue = await simple.retrieve()
     console.log(`Update Value is: ${updatedValue}`)
+    console.log("Do you know handsome guy")
+    const handsomeGuy = await simple.seeWhoHandsome()
+    console.log(`=> ${handsomeGuy}`)
+    // console.log(updatedValue)            // BigNumber { value: "99" }
+    // console.log(typeof updatedValue)     // this guy is object
+    // console.log(typeof handsomeGuy)      // this guy is string
 }
 
-// args for contract that have to pass args in constructor
 const verify = async (contractAddress, args) => {
     console.log("Verifying contract... ")
     try {
