a188ef2bae23aa44e4ed4ee3eb179ddd6049a27c
Nawakoon Peerarasee <fame.8@hotmail.com>
2022-06-13 16:41:41 +0700

    Build gas-report

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block_number")
+require("hardhat-gas-reporter")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -22,9 +23,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETERSCAN_API_KEY
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -41,6 +43,15 @@ module.exports = {
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        // token: "MATIC", // to see gas report on MATIC chain
+        // https://www.npmjs.com/package/hardhat-gas-reporter
     },
     solidity: "0.8.8",
 }
