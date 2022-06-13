1b42d42a0ad131168535e5a72fcd311cbb3dde58
Nawakoon Peerarasee <fame.8@hotmail.com>
2022-06-13 15:35:08 +0700

    comment

--- test/test_deploy.js
+++ test/test_deploy.js
@@ -5,18 +5,26 @@ const { expect, assert } = require("chai")
 // do this as annonymous function
 // write annonymous like this is the best practice
 describe("SimpleStorage", function () {
-    let SimpleStorageFactory, simpleStorage
+    // make these variable global
+    let simpleStorageFactory, simpleStorage
     beforeEach(async function () {
         simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
         simpleStorage = await simpleStorageFactory.deploy()
     })
 
+    // try this for running specific test
+    // $ yarn hardhat test --grep <test name>
+    // $ yarn hardhat test --grep favorite
     it("Should start with a favorite number: 0", async function () {
         const currentValue = await simpleStorage.retrieve()
         const expectedValue = "0"
         // assert
         assert.equal(currentValue.toString(), expectedValue)
+        // this work the same
+        // expect(currentValue.toString()).to.equal(expectedValue)
     })
+    // try this for running specific test
+    // it.only("Should update when we call store", async function () {
     it("Should update when we call store", async function () {
         const expectedValue = "65"
         const transactionResponse = await simpleStorage.store(expectedValue)
